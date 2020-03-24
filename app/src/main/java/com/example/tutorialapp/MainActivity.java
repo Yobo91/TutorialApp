1682d321346c7238a4a784b4e2c0ac19bdf4daad
Philipp <philipp1501@gmx.de>
2020-03-24 08:26:49 +0100

    Button erzeugt

--- app/src/main/java/com/example/tutorialapp/MainActivity.java
+++ app/src/main/java/com/example/tutorialapp/MainActivity.java
@@ -1,7 +1,9 @@
 package com.example.tutorialapp;
 
+import android.view.View;
 import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +11,14 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        final Button button = (Button) this.findViewById(R.id.button_id);
+
+        button.setOnClickListener(
+                new Button.OnClickListener() {
+                    public void onClick(View v){
+                        button.setText("You did it!!!");
+                    }
+                }
+        );
     }
 }
