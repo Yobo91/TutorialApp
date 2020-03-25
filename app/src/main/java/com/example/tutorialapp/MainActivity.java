461ed2f2a219a058ab77d57bca6a04e48d939dc7
Philipp <philipp1501@gmx.de>
2020-03-25 16:59:38 +0100

    gesten

--- app/src/main/java/com/example/tutorialapp/MainActivity.java
+++ app/src/main/java/com/example/tutorialapp/MainActivity.java
@@ -1,33 +1,50 @@
 package com.example.tutorialapp;
 
+import android.view.MotionEvent;
 import android.view.View;
+import android.widget.TextView;
 import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
-import android.widget.Button;
-import android.widget.EditText;
-import android.widget.TextView;
+import androidx.core.view.MotionEventCompat;
+
 
 public class MainActivity extends AppCompatActivity {
+    TextView txt;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        final Button button = (Button) this.findViewById(R.id.button_id);
-        final EditText groesse = (EditText) this.findViewById(R.id.nu_groesse_id);
-        final EditText gewicht = (EditText) this.findViewById(R.id.nu_gewicht_id);
+        txt = (TextView) this.findViewById(R.id.text_id);
+        txt.setOnTouchListener(new View.OnTouchListener() {
+            @Override
+            public boolean onTouch(View v, MotionEvent event) {
+                int action = MotionEventCompat.getActionMasked(event);
 
-        final TextView out = (TextView) this.findViewById(R.id.out_id);
-        button.setOnClickListener(
-                new Button.OnClickListener() {
-                    public void onClick(View v){
-                        double bmi = Double.parseDouble(gewicht.getText().toString());
-                        bmi /= (Double.parseDouble(groesse.getText().toString())/100);
-                        bmi /= (Double.parseDouble(groesse.getText().toString())/100);
-
-                        out.setText(((Double) bmi).toString());
-                    }
+                switch (action) {
+                    case (MotionEvent.ACTION_DOWN):
+                        txt.setText("Pressed the TXT");
+                    case (MotionEvent.ACTION_MOVE):
+                        txt.setText("I got to");
                 }
-        );
+                return true;
+            }
+        });
+    }
+
+    @Override
+    public boolean onTouchEvent(MotionEvent event) {
+        int action = MotionEventCompat.getActionMasked(event);
+
+        switch (action) {
+            case (MotionEvent.ACTION_DOWN):
+                txt.setText("Pressed");
+                return true;
+            case (MotionEvent.ACTION_MOVE):
+                txt.setText("Move It Move It");
+                return true;
+            default:
+                return super.onTouchEvent(event);
+        }
     }
 }
