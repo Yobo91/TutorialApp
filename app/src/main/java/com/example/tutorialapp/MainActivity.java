9113e0ac52e728e71aeefcf409259b3046ff0006
Philipp <philipp1501@gmx.de>
2020-03-31 16:43:49 +0200

    gesten

--- app/src/main/java/com/example/tutorialapp/MainActivity.java
+++ app/src/main/java/com/example/tutorialapp/MainActivity.java
@@ -1,50 +1,84 @@
 package com.example.tutorialapp;
 
+import android.view.GestureDetector;
 import android.view.MotionEvent;
 import android.view.View;
 import android.widget.TextView;
 import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
+import androidx.core.view.GestureDetectorCompat;
 import androidx.core.view.MotionEventCompat;
 
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements GestureDetector.OnDoubleTapListener, GestureDetector.OnGestureListener {
     TextView txt;
+    private GestureDetectorCompat compat;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         txt = (TextView) this.findViewById(R.id.text_id);
-        txt.setOnTouchListener(new View.OnTouchListener() {
-            @Override
-            public boolean onTouch(View v, MotionEvent event) {
-                int action = MotionEventCompat.getActionMasked(event);
-
-                switch (action) {
-                    case (MotionEvent.ACTION_DOWN):
-                        txt.setText("Pressed the TXT");
-                    case (MotionEvent.ACTION_MOVE):
-                        txt.setText("I got to");
-                }
-                return true;
-            }
-        });
+        this.compat = new GestureDetectorCompat(this, this);
+        compat.setOnDoubleTapListener(this);
     }
 
     @Override
     public boolean onTouchEvent(MotionEvent event) {
-        int action = MotionEventCompat.getActionMasked(event);
+        this.compat.onTouchEvent(event);
+        return super.onTouchEvent(event);
+    }
 
-        switch (action) {
-            case (MotionEvent.ACTION_DOWN):
-                txt.setText("Pressed");
-                return true;
-            case (MotionEvent.ACTION_MOVE):
-                txt.setText("Move It Move It");
-                return true;
-            default:
-                return super.onTouchEvent(event);
-        }
+    //Gesten//
+    @Override
+    public boolean onSingleTapConfirmed(MotionEvent e) {
+        txt.setText(e.toString());
+        return true;
+    }
+
+    @Override
+    public boolean onDoubleTap(MotionEvent e) {
+        txt.setText(e.toString());
+        return true;
+    }
+
+    @Override
+    public boolean onDoubleTapEvent(MotionEvent e) {
+        txt.setText(e.toString());
+        return true;
+    }
+
+    @Override
+    public boolean onDown(MotionEvent e) {
+        txt.setText(e.toString());
+        return true;
+    }
+
+    @Override
+    public void onShowPress(MotionEvent e) {
+        txt.setText(e.toString());
+    }
+
+    @Override
+    public boolean onSingleTapUp(MotionEvent e) {
+        txt.setText(e.toString());
+        return true;
+    }
+
+    @Override
+    public boolean onScroll(MotionEvent e1, MotionEvent e2, float distanceX, float distanceY) {
+        txt.setText(e1.toString() + "\n" + e2.toString());
+        return true;
+    }
+
+    @Override
+    public void onLongPress(MotionEvent e) {
+        txt.setText(e.toString());
+    }
+
+    @Override
+    public boolean onFling(MotionEvent e1, MotionEvent e2, float velocityX, float velocityY) {
+        txt.setText(e1.toString() + "\n" + e2.toString());
+        return true;
     }
 }
